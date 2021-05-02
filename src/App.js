e81fc020d07ead5889766b7ffd9da481824a3b72
Jalal Mallah <jalal_mallah@live.com>
2021-05-02 19:38:43 +0200

    style: style the app for both mobile and desktop devices

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useEffect } from 'react';
+
 import 'styles/global.scss';
 
-function App() {
-  return <div>Hola, mundo! 👋</div>;
-}
+const App = () => {
+  const url = 'https://type.fit/api/quotes';
+
+  useEffect(() => {
+    console.log(url);
+  }, []);
+
+  return (
+    <div className='container'>
+      <h1 className='title'>Quote Generator</h1>
+      <p className='quote-text'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit odit mollitia
+        incidunt deleniti. Et aliquam voluptatem placeat illo cum quia ducimus officiis obcaecati.
+        Corrupti quae, repellat deleniti saepe natus aliquid?
+      </p>
+      <p className='quote-author'>John Doe</p>
+      <button className='generate-quote'>Generate New Quote</button>
+    </div>
+  );
+};
 
 export default App;
